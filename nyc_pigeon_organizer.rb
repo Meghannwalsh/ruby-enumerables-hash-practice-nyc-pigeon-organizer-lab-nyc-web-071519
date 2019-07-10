e9655eca84d504e5a597ec9b8dc8e0653b5bc0e0
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-10 14:55:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,16 @@
 def nyc_pigeon_organizer(data)
   pigeon_data = {}
   data.each do |k, v|
-    v.each do |name|
+    v.each do |spac|
+      spac.each do |name|
       if !pigon_data.include?(name)
         pigeon_data[name] = {:color => [], :gender => [], :lives => []}
       end
     end
   end
-  data.each
+  data.each do |k, v|
+    v.each do |spac|
+        if spac.include?()
+
 
 end
